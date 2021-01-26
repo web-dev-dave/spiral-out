ec6ec77159f96bf1b829cfae94ec7194db71ce12
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-26 13:12:52 +1300

    add bg to model

--- app.js
+++ app.js
@@ -120,6 +120,14 @@
     floor.receiveShadow = true;
     floor.position.y = -11;
     scene.add(floor);
+
+    let geometry = new THREE.SphereGeometry(8, 32, 32);
+    let material = new THREE.MeshBasicMaterial({ color: 0x9bffaf }); // Change colour
+    let sphere = new THREE.Mesh(geometry, material);
+    sphere.position.z = -15;
+    sphere.position.y = -2.5;
+    sphere.position.x = -0.25;
+    scene.add(sphere);
   }
 
   function update() {
